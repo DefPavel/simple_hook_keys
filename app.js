b762753ee85a296dcd3548b642209264ac9f7d4b
DefPavel <33838515+DefPavel@users.noreply.github.com>
2021-12-23 16:55:25 +0300

    tesing

--- app.js
+++ app.js
@@ -1,40 +1,68 @@
-'use strict';
 const ioHook = require('iohook');
+const StringBuilder = require('node-stringbuilder');
+
 let element = '';
-//const arrayElemet = [];
+let decoded = '';
+
+function ReplaceData(params) {
+   /* Код Numpud :
+    71 - Num7
+    72 - Num8
+    73 - Num9
+    75 - Num4
+    76 - Num5
+    79 - Num1
+    80 - Num2
+    81 - Num3
+    82 - Num0
+ */
+    const sb = new StringBuilder(params);
+    
+    sb.replaceAll("7572", "fak");
+    /*sb.replace("49", "1");
+    sb.replace("50", "2");
+    sb.replace("51", "3");
+    sb.replace("52", "4");
+    sb.replace("53", "5");
+    sb.replace("54", "6");
+    sb.replace("55", "7");
+    sb.replace("56", "8");
+    sb.replace("57", "9");
+    */
+   return sb.toString().trim();
+}
+
 ioHook.on('keydown', key => {
-  /*
-    Вообще нужен Numpad - но у меня его нет =))
-    Поэтому накидаю пример просто на обычных цифрах
-  */
-  /*
-    Код цифры :  
-    1 = 2;
-    2 = 3;
-    3 = 4;
-    4 = 5;
-    5 = 6;
-    6 = 7;
-    7 = 8;
-    8 = 9;
-    9 = 10;
-    0 = 11;
-  */
-  
-  if(key.keycode >= 2 && key.keycode <= 11) {
+
+  if(key.keycode >= 71 && key.keycode <= 82) {
 
     element += key.keycode;
-    if(element.length == 8) {
-      console.log(element);
-      // Кодинг
+    for (let index = 0; index < element.length; index ++) {
+
+      console.log(element[index].substring(index , 2));
+      //element += ReplaceData(key.keycode.substring(2));  
     }
-    /*arrayElemet.push(key.keycode);
-    if(arrayElemet.length == 8) {
-      //Кодинг
-      console.log(arrayElemet);
+    
+    /*for (let index = 0; index < element.length; index ++) {
+      console.log("ttt :" + element[index]); 
+      decoded += ReplaceData(element[index].substring(index, 2));
     }
     */
-  }
+  
+    /*if(element.length === 20) {
+
+      // Обрезаем 000
+      for (let index = 6; index < element.length; index += 2) {
+        const test = element[index];
+
+        console.log(test);
+        //decoded += ReplaceCode(textBox1.Text.Substring(i, 4));
+
+      }
+    }
+    */
+  } 
+  //console.log(element);
     
 })
 
