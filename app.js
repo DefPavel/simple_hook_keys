cafa56d35c04c7eb91f9e4ab2a20f4aee29c49c8
DefPavel <33838515+DefPavel@users.noreply.github.com>
2021-12-24 10:26:31 +0300

    рабочая схема глобального хука

--- app.js
+++ app.js
@@ -5,44 +5,45 @@ let element = '';
 let decoded = '';
 
 ioHook.on('keydown', key => {
-  
-  /*if(key.keycode >= 71 && key.keycode <= 82) {
   // Выбираем диапазон только Numpad
-    //element += key.keycode;
+  if(key.keycode >= 71 && key.keycode <= 82) {
+    //Записываем весь объект
+    element += key.keycode;
 
-    console.log(key.keycode);
-      // Записываем до
-      if(element.length === 30) {
-        // Проходимся по записанному элементу
-        for (let index = 12; index < element.length; index += 4) {
-          //console.log("element :" + element.substring(index, 2)); 
-          decoded += ReplaceData(element.substring(index, 4));
-        }   
-      }
-      
-      
-  }*/
-  console.log(key.keycode);
+    if(element.length === 40) {
+      // Пропускаем первые три символа 000
+       for (let index = 12; index < element.length; index += 4) {
+        // Заменяем на адекватный номер пропуска
+        decoded += ReplaceData(element.substr(index, 4));
+        //console.log(element.substr(index, 4));
+       }
+       console.log('CARD-ID:' + decoded);
+       
+       /* 
+          Делаем какие-то манипуляции с CARD-ID
+       */
+
+
+       // Очищаем элемент
+       element = '';
+       decoded = '';
+    }
+  }
 });
 
 function ReplaceData(params) {
-  /* Код Numpud :
-   71 - Num7
-   72 - Num8
-   73 - Num9
-   75 - Num4
-   76 - Num5
-   79 - Num1
-   80 - Num2
-   81 - Num3
-   82 - Num0
-*/
-  const sb = new StringBuilder(params);
-  sb.replaceAll("7572", "0");
-  /*sb.replaceAll("7375", "1");
-  sb.replaceAll("7675", "2");
-  */
 
+  const sb = new StringBuilder(params);
+  sb.replaceAll('7572', '0'); 
+  sb.replaceAll('7573', '1');
+  sb.replaceAll('7682', '2');
+  sb.replaceAll('7679', '3');
+  sb.replaceAll('7680', '4');
+  sb.replaceAll('7681', '5'); 
+  sb.replaceAll('7675', '6');
+  sb.replaceAll('7676', '7');
+  sb.replaceAll('7677', '8');
+  sb.replaceAll('7671', '9');
   return sb.toString().trim();
 }
 
